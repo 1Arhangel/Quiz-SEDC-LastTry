04a1b3caa6063dbe0919806af59ad0e838c35940
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-10-04 21:13:01 +0200

    Added YES/NO Class

---
+++ SEDCQuiz/SEDCQuiz.Domain.Entities/YesNo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDCQuiz.Domain.Entities
+{
+    public class YesNo
+    {
+        public int ID { get; set; }
+    }
+}
