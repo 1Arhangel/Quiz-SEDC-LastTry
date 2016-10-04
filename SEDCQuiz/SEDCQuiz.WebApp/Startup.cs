2f261911bff3bb7186729350017a1b078462992c
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-10-04 20:59:09 +0200

    Initial Commit

---
+++ SEDCQuiz/SEDCQuiz.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEDCQuiz.WebApp.Startup))]
+namespace SEDCQuiz.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
